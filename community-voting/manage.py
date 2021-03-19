9faa26d871bb006de0259e395f0527f84f9a7631
Razvan STANCIOIU <stancioiu.razvan@gmail.com>
2021-03-19 01:48:17 +0100

    Update manage py and fix RUST version issue

--- community-voting/manage.py
+++ community-voting/manage.py
@@ -45,6 +45,17 @@ def execute_command(command):
         exit(0)
 
 
+def install():
+    RUST_VERSION = "nightly-2021-02-26"
+    INSTALL_COMMAND = "erdpy deps install rust --overwrite --tag %s" 
+    execute_command(INSTALL_COMMAND % RUST_VERSION)
+
+
+def clean():
+    CLEAN_COMMAND = "erdpy --verbose contract clean \"%s\""
+    execute_command(CLEAN_COMMAND % os.getcwd())
+
+
 def build():
     BUILD_COMMAND = "erdpy --verbose contract build \"%s\""
     execute_command(BUILD_COMMAND % os.getcwd())
@@ -57,14 +68,21 @@ def test():
 
 def main():
     parser = argparse.ArgumentParser()
+    parser.add_argument('--install', dest='install', action='store_true', default=False)
+    parser.add_argument('--clean', dest='clean', action='store_true', default=False)
     parser.add_argument('--build', dest='build', action='store_true', default=True)
     parser.add_argument('--test', dest='test', action='store_true', default=True)
     args = parser.parse_args()
     setup_environment()
-    if args.build:
-        build()
-    if args.test:
-        test()
+    if args.install:
+        install()
+    elif args.clean:
+        clean()
+    else:
+        if args.build:
+            build()
+        if args.test:
+            test()
 
 
 if __name__ == "__main__":
