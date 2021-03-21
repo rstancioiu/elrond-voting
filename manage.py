52173ea79a97656d6149d453b1c9da92690636a6
Razvan STANCIOIU <stancioiu.razvan@gmail.com>
2021-03-22 00:59:45 +0100

    Improve building tools

--- manage.py
+++ manage.py
@@ -1,16 +1,21 @@
 #!/usr/bin/env python3
 
 import argparse
+import json
 import os
 import subprocess
+
+from typing import List
 
 
 CYAN = "\033[96m {}\033[00m"
 GREEN = "\033[92m {}\033[00m"
 RED = "\033[91m {}\033[00m"
 
+CONFIGURATION = json.load(open(".managerc"))
 
-def add_environment_variable(environment, key, value):
+
+def add_environment_variable(environment: dict, key: str, value: str):
     if key in environment:
         environment[key] = "%s:%s" % (value, environment[key])
     else:
@@ -31,7 +36,7 @@ def setup_environment():
     return environment
 
 
-def execute_command(command):
+def execute_command(command: str):
     environment = setup_environment()
     print(CYAN.format("> %s" % command))
     process = subprocess.Popen(command, shell=True, env=environment, stdout=subprocess.PIPE)
@@ -45,26 +50,47 @@ def execute_command(command):
         exit(0)
 
 
-def build():
-    BUILD_COMMAND = "erdpy --verbose contract build \"%s\""
-    execute_command(BUILD_COMMAND % os.getcwd())
+def install():
+    INSTALL_COMMAND = "erdpy deps install rust --overwrite --tag %s" 
+    execute_command(INSTALL_COMMAND % CONFIGURATION['rust_version'])
 
 
-def test():
-    TEST_COMMAND = "mandos-test \"%s\""
-    execute_command(TEST_COMMAND % os.getcwd())
+def clean(contracts: List[str]):
+    CLEAN_COMMAND = "erdpy --verbose contract clean %s"
+    for contract in contracts:
+        execute_command(CLEAN_COMMAND % contract)
+
+
+def build(contracts: List[str]):
+    BUILD_COMMAND = "cd %s && erdpy --verbose contract build"
+    for contract in contracts:
+        execute_command(BUILD_COMMAND % (contract))
+
+
+def test(contracts: List[str]):
+    TEST_COMMAND = "cd %s && mandos-test ."
+    for contract in contracts:
+        execute_command(TEST_COMMAND % contract)
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--build', dest='build', action='store_true', default=True)
-    parser.add_argument('--test', dest='test', action='store_true', default=True)
+    parser.add_argument('--install', dest='install', action='store_true', default=False)
+    parser.add_argument('--clean', dest='clean', action='store_true', default=False)
+    parser.add_argument('--build', dest='build', action='store_true', default=False)
+    parser.add_argument('--test', dest='test', action='store_true', default=False)
+    parser.add_argument('--contracts', dest='contracts', default=CONFIGURATION['contracts'])
     args = parser.parse_args()
     setup_environment()
-    if args.build:
-        build()
-    if args.test:
-        test()
+    if args.install:
+        install()
+    elif args.clean:
+        clean(args.contracts)
+    else:
+        if args.build:
+            build(args.contracts)
+        if args.test:
+            test(args.contracts)
 
 
 if __name__ == "__main__":
